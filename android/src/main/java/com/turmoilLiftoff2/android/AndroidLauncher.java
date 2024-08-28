afb8ade99ffc8d9a8623cd6ad2796633d362eabf
pajchuga <pajcepre@gmail.com>
2024-08-28 20:33:11 +0200

    gradle fuckery

---
+++ android/src/main/java/com/turmoilLiftoff2/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.turmoillift2.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.turmoillift2.TurmoilLiftoff2;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new TurmoilLiftoff2(), configuration);
+    }
+}
